4f844b6446dca46e1f07f609a471ea3aad349ac5
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-06 21:01:03 +0900

    feat: 구간 추가 기능 구현 및 추가 관련 검증 구현

--- src/main/java/wooteco/subway/line/dto/LineResponse.java
+++ src/main/java/wooteco/subway/line/dto/LineResponse.java
@@ -1,20 +1,30 @@
 package wooteco.subway.line.dto;
 
 import wooteco.subway.line.domain.Line;
+import wooteco.subway.station.dto.StationResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LineResponse {
     private final Long id;
     private final String name;
     private final String color;
+    private final List<StationResponse> stations;
 
-    public LineResponse(Line line) {
-        this(line.getId(), line.getName(), line.getColor());
+    public LineResponse(Line line, List<StationResponse> stations) {
+        this(line.getId(), line.getName(), line.getColor(), stations);
     }
 
-    public LineResponse(Long id, String name, String color) {
+    public LineResponse(Line line) {
+        this(line.getId(), line.getName(), line.getColor(), new ArrayList<>());
+    }
+
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.stations = stations;
     }
 
     public Long getId() {
@@ -27,5 +37,9 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
     }
 }
